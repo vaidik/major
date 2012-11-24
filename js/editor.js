eb80ca4d5d7959c9291dc5f930980d7151f1f87b
Vaidik Kapoor <kapoor.vaidik@gmail.com>
2012-11-24 11:48:56 +0530

    tiny_mce supports autocomplete the way i want (not like FB)

--- js/editor.js
+++ js/editor.js
@@ -3,13 +3,15 @@ $(document).ready(function() {
         mode : "textareas",
         language: false,
         theme : "advanced",
-        plugins: "fullpage",
+        plugins : "autocomplete",
         theme_advanced_buttons1 : "bold,italic,underline,separator,strikethrough,justifyleft,justifycenter,justifyright,justifyfull,bullist,numlist,undo,redo,link,unlink",
         theme_advanced_buttons2 : "",
         theme_advanced_buttons3 : "",
-        theme_advanced_buttons3_add : "fullpage",
+        theme_advanced_buttons3_add : "",
         theme_advanced_toolbar_location : "top",
         theme_advanced_toolbar_align : "left",
-        theme_advanced_statusbar_location : ""
+        theme_advanced_statusbar_location : "",
+
+        autocomplete_options: "john,jane,william",
     });
 });
