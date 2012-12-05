cbce30d1ae6be3a2ac74f3a8f5c77853315b4aec
Vaidik Kapoor <kapoor.vaidik@gmail.com>
2012-12-05 16:25:01 +0530

    fixed

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/*
 var static = require('node-static');
 
 //
@@ -14,3 +15,21 @@ require('http').createServer(function (request, response) {
     file.serve(request, response);
   });
 }).listen(8000);
+*/
+
+var app = require('express')()
+  , server = require('http').createServer(app)
+  , io = require('socket.io').listen(server);
+
+server.listen(8000);
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
